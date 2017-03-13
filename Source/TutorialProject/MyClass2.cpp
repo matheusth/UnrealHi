34f19112b62d8c1dfccd2b5fdffb0e7adb446b9e
Matheus Henrique <Matheus Henrique>
2017-03-13 14:05:59 -0300

    Adding project files

---
+++ Source/TutorialProject/MyClass2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TutorialProject.h"
+#include "MyClass2.h"
+
+MyClass2::MyClass2()
+{
+}
+
+MyClass2::~MyClass2()
+{
+}
---
+++ Source/TutorialProject/TutorialProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TutorialProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIALPROJECT_API ATutorialProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
